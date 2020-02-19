6480871764c033bf9210bca245bebb49bbfdda13
AmorphousCube <amorphouscube@gmail.com>
2020-02-19 14:41:11 -0500

    I legit forgot this was a git project

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,15 +1,108 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment/index"
 import "components/Application.scss";
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "7pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "10pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },{
+    id: 4,
+    time: "11pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },{
+    id: 5,
+    time: "12pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+];
+
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/api/days').then((res) => {
+      console.log("before",days);
+      console.log("res",res.data);
+      setDays(res.data);
+      console.log("after",days);
+    })
+  }, [] )
+
   return (
     <main className="layout">
+      
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((app) => {
+          return (
+            <Appointment key={app.id}
+                          {...app}
+                          />
+          )
+        })}
+        <Appointment key="last" time="13pm" />
       </section>
     </main>
   );
---
+++ src/components/DayListItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+const classNames = require('classnames');
+
+const formatSpots = function(spots) {
+  if (spots) {
+    if(spots === 1){
+      return "1 spot remaining";
+    }
+    else {
+      return spots + " spots remaining";
+    }
+  } else{
+    return "no spots remaining";
+  }
+};
+
+export default function DayListItem(props) {
+  let dayClass = classNames({
+    "day-list__item" : true,
+    "day-list__item--selected" : props.selected, 
+    "day-list__item--full" : props.spots===0
+  });
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2>{props.name}</h2>
+      <h3>{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
